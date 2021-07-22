96171b29cca4a54666470d19483c62ef74f657a0
Nick Castle <castle5896@gmail.com>
2021-07-22 10:32:54 -0400

    allow a user to search through characters by name

--- pages/rick-and-morty/index.vue
+++ pages/rick-and-morty/index.vue
@@ -3,26 +3,60 @@
     <NavBar />
     <h1 class="center header">Rick and Morty page</h1>
 
-    <div class="container">
-      <CharacterCard
-        v-for="char in characters"
-        :key="char.id"
-        :characterInfo=char
-      />
+    <h3 class="center">Search characters by name:</h3>
+    <div class="center">
+      <input @change="search" type="text" v-model="input">
+      <button @click="search">Search</button>
+      <button @click="reset">Reset</button>
     </div>
+
+    <div class="container">
+
+      <template v-if="filteredCharacters">
+        <CharacterCard
+          v-for="char in filteredCharacters"
+          :key="char.id"
+          :characterInfo=char
+        />
+      </template>
+
+      <template v-else>
+        <CharacterCard
+          v-for="char in characters"
+          :key="char.id"
+          :characterInfo=char
+        />
+      </template>
+
+    </div>
+
   </div>
 </template>
 
 <script>
 export default {
-  async created () {
-    await this.$store.dispatch('fetchCharacters', { self: this })
-    console.log('printing after created / dispatch?')
+  data () {
+    return {
+      input: '',
+      filteredCharacters: null
+    }
   },
-
   computed: {
     characters () {
       return this.$store.getters.getAllCharacters
+    }
+  },
+  methods: {
+    search () {
+      this.filteredCharacters = this.characters.filter((char) => {
+        const name = char.name.toLowerCase()
+        const input = this.input.toLowerCase()
+        return name.includes(input)
+      })
+    },
+    reset () {
+      this.filteredCharacters = null
+      this.input = ''
     }
   }
 }
@@ -39,10 +73,10 @@ export default {
     margin-right: auto;
   }
   .container {
+    margin: 1rem auto;
     width: 960px;
     display: flex;
     flex-wrap: wrap;
-    margin: auto;
     padding: 1rem;
     background-color: grey;
     align-items: center;
