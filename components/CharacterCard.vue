b8d59ac166ef4f9ad04cd75720b25649cfa2a7ca
Nick Castle <castle5896@gmail.com>
2021-07-21 20:06:21 -0400

    show a basic query to an api endpoint and display the list of returned data

---
+++ components/CharacterCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="card">
+    <h3 class="name">{{ characterInfo.name }}</h3>
+    <img :src="characterInfo.image" alt={} class="image">
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['characterInfo']
+}
+</script>
+
+<style lang="scss" scoped>
+  .card {
+    width: 200px;
+    height: 200px;
+    border: 2px solid black;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+    background-color: #e0e0e0;
+    margin: 1rem;
+    padding: 1rem;
+  }
+  .image {
+    width: 100px;
+    height: 100px;
+    border-radius: 1rem;
+  }
+  .name {
+    text-align: center;
+  }
+</style>
--- pages/rick-and-morty.vue
+++ pages/rick-and-morty.vue
@@ -1,12 +1,51 @@
 <template>
-  <main>
+  <div>
     <NavBar />
-    <h1>Rick and Morty page</h1>
-  </main>
+    <h1 class="center header">Rick and Morty page</h1>
+
+    <div class="container">
+      <CharacterCard
+        v-for="char in characters"
+        :key="char.id"
+        :characterInfo=char
+      />
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
+  async asyncData ({ $http }) {
+    // base url to rick and morty api endpoint
+    const baseUrl = 'https://api.sampleapis.com/rickandmorty/'
 
+    // get data from each of the characters api endpoint
+    const characters = await $http.$get(baseUrl + 'characters')
+    console.log({ characters })
+
+    return { characters }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+  .header {
+    margin-top: 1rem;
+    margin-bottom: 2rem;
+  }
+  .center {
+    width: fit-content;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  .container {
+    width: 960px;
+    display: flex;
+    flex-wrap: wrap;
+    margin: auto;
+    padding: 1rem;
+    background-color: grey;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
